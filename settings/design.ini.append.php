9ff2c21ad8d9831c41b7c276b9fd46e59f8a68be
Leif Arne Storset <leifarne@storset.net>
2011-12-07 22:42:23 +0100

    Add new BackendJavaScriptList entries for admin2

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -5,10 +5,16 @@ DesignExtensions[]=ntags
 [StylesheetSettings]
 CSSFileList[]=jquery/redmond/jquery-ui-1.7.2.custom.css
 CSSFileList[]=ntags.css
+BackendCSSFileList[]=jquery/redmond/jquery-ui-1.7.2.custom.css
+BackendCSSFileList[]=ntags.css
 
 [JavaScriptSettings]
 JavaScriptList[]=namespace.js
 JavaScriptList[]=jquery-1.3.2.min.js
 JavaScriptList[]=ntags.js
 JavaScriptList[]=labs_json.js
+BackendJavaScriptList[]=namespace.js
+BackendJavaScriptList[]=jquery-1.3.2.min.js
+BackendJavaScriptList[]=ntags.js
+BackendJavaScriptList[]=labs_json.js
 */ ?>
