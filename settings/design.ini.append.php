cb1e974e27593a0c1fa0444024f71c3fd7542866
Leif Arne Storset <leifarne@storset.net>
2012-01-19 22:48:07 +0100

    Remove references in settings to jQuery

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -4,11 +4,9 @@ DesignExtensions[]=ntags
 
 [JavaScriptSettings]
 JavaScriptList[]=namespace.js
-JavaScriptList[]=jquery-1.3.2.min.js
 JavaScriptList[]=ntags.js
 JavaScriptList[]=labs_json.js
 BackendJavaScriptList[]=namespace.js
-BackendJavaScriptList[]=jquery-1.3.2.min.js
 BackendJavaScriptList[]=ntags.js
 BackendJavaScriptList[]=labs_json.js
 */ ?>
