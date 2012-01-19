a7c1f433a6416e5f3e0c9f1efec219ffe5c39175
Leif Arne Storset <leifarne@storset.net>
2012-01-19 22:49:36 +0100

    Move AJAX server classes to classes directory

--- modules/ntags/ajax.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-Causes a clean eZ publish exit in order to prevent debug messages and remembering the AJAX url (if, for instance, the cache is cleared immediately after the AJAX execution). Include in every AJAX file.
-*/
-function ajax_shutdown() {
-    include_once( 'lib/ezutils/classes/ezexecution.php' );
-    eZExecution::cleanExit();
-}
-?>
